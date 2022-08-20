fad0bbb631866821bcf9e75ba5386a11cf75c2e6
RitiKSiBJr <letter.ritik060@gmail.com>
2022-08-20 18:28:20 +0545

    commited

---
+++ rock_paper.py
@@ -0,0 +1,19 @@
+
+user_input = str(input("Choose 'r' for rock, 's' for scissor and 'p' for paper: ").lower())
+
+import random
+
+
+computer = random.choice(['r','p','s'])
+print(computer)
+
+if user_input == computer[0]:
+    print("It\'s a tie")
+
+else:
+    if (user_input == 's' and computer[0] == 'p') or (user_input == 'r' and computer[0] == 's') or\
+        (user_input == 'p' and computer[0] == 'r') :
+        print("You won")
+
+    else:
+        print("You loss")
