a398f12cefea422471708527fa386ff76e165a4e
RitiKSiBJr <letter.ritik060@gmail.com>
2022-12-01 16:21:19 +0545

    changed

--- rock_paper.py
+++ rock_paper.py
@@ -3,17 +3,20 @@ user_input = str(input("Choose 'r' for rock, 's' for scissor and 'p' for paper: 
 
 import random
 
+def main():
+    computer = random.choice(['r','p','s'])
+    print(computer)
 
-computer = random.choice(['r','p','s'])
-print(computer)
-
-if user_input == computer[0]:
-    print("It\'s a tie")
-
-else:
-    if (user_input == 's' and computer[0] == 'p') or (user_input == 'r' and computer[0] == 's') or\
-        (user_input == 'p' and computer[0] == 'r') :
-        print("You won")
+    if user_input == computer[0]:
+        print("It\'s a tie")
 
     else:
-        print("You loss")
+        if (user_input == 's' and computer[0] == 'p') or (user_input == 'r' and computer[0] == 's') or\
+            (user_input == 'p' and computer[0] == 'r') :
+            print("You won")
+
+        else:
+            print("You loss")
+
+if __name__ == "__main__":
+    main()
